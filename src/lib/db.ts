3b261240ab092e52c2deea5362ff14ba08e145f5
ddrunkgoose <ddrunkgoose@protonmail.com>
2024-08-10 23:37:09 +0800

    refactor: db now using singleton pattern

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -12,72 +12,77 @@ export interface HistoryData {
 }
 
 class HistoryDb extends Dexie {
-  public history!: Table<HistoryData, number>;
+  private history!: Table<HistoryData, number>;
+  private record!: Set<number>;
 
-  public constructor() {
+  constructor() {
     super('PdlHistory');
     this.version(3).stores({
       history: 'pid, userId, user, title, comment, *tags'
     });
+
+    logger.time('loadDb');
+    this.history.toArray().then((datas) => {
+      this.record = new Set(datas.map((data) => data.pid));
+      logger.timeEnd('loadDb');
+    });
+  }
+
+  public async add(historyData: HistoryData) {
+    if (!(await this.has(historyData.pid))) {
+      this.history.put(historyData);
+      this.record.add(historyData.pid);
+    }
+  }
+
+  public bulkAdd(historyDatas: HistoryData[]) {
+    const result = this.history.bulkPut(historyDatas);
+    historyDatas.forEach((data) => {
+      this.record.add(data.pid);
+    });
+    return result;
+  }
+
+  public async has(pid: number | string): Promise<boolean> {
+    if (typeof pid === 'string') pid = Number(pid);
+    if (this.record) {
+      return this.record.has(pid);
+    } else {
+      return !!(await this.history.get(pid));
+    }
+  }
+
+  public getAll(): Promise<HistoryData[]> {
+    return this.history.toArray();
+  }
+
+  public generateCsv(): Promise<Blob> {
+    return this.getAll().then((datas) => {
+      const csvData: string[][] = datas.map((historyData) => {
+        const { pid, userId = '', user = '', title = '', tags = '', comment = '' } = historyData;
+        return [String(pid), String(userId), user, title, comment, tags ? tags.join(',') : tags];
+      });
+      csvData.unshift(['id', 'userId', 'user', 'title', 'comment', 'tags']);
+
+      return generateCsv(csvData);
+    });
+  }
+
+  public clear() {
+    this.record && (this.record = new Set());
+    return this.history.clear();
   }
 }
 
-function createHistoryDb() {
-  const db = new HistoryDb();
-  let record: Set<number>;
-
-  logger.time('loadDb');
-  db.history.toArray().then((datas) => {
-    record = new Set(datas.map((data) => data.pid));
-    logger.timeEnd('loadDb');
-  });
-
-  return {
-    async add(historyData: HistoryData) {
-      if (!(await this.has(historyData.pid))) {
-        db.history.put(historyData);
-        record.add(historyData.pid);
-      }
-    },
-
-    bulkAdd(historyDatas: HistoryData[]) {
-      const result = db.history.bulkPut(historyDatas);
-      historyDatas.forEach((data) => {
-        record.add(data.pid);
-      });
-      return result;
-    },
-
-    async has(pid: number | string): Promise<boolean> {
-      if (typeof pid === 'string') pid = Number(pid);
-      if (record) {
-        return record.has(pid);
-      } else {
-        return !!(await db.history.get(pid));
-      }
-    },
-
-    getAll(): Promise<HistoryData[]> {
-      return db.history.toArray();
-    },
-
-    generateCsv(): Promise<Blob> {
-      return this.getAll().then((datas) => {
-        const csvData: string[][] = datas.map((historyData) => {
-          const { pid, userId = '', user = '', title = '', tags = '', comment = '' } = historyData;
-          return [String(pid), String(userId), user, title, comment, tags ? tags.join(',') : tags];
-        });
-        csvData.unshift(['id', 'userId', 'user', 'title', 'comment', 'tags']);
-
-        return generateCsv(csvData);
-      });
-    },
-
-    clear() {
-      record && (record = new Set());
-      return db.history.clear();
+let instance: HistoryDb;
+const SingletonHistoryDb = new Proxy(HistoryDb, {
+  construct(target) {
+    if (!instance) {
+      return (instance = new target());
     }
-  };
-}
 
-export const historyDb = createHistoryDb();
+    return instance;
+  }
+});
+
+export const historyDb = new SingletonHistoryDb();
