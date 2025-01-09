4ebc05d333f38bd94bf3da99a359fd05b4e230bb
ddrunkgoose <ddrunkgoose@protonmail.com>
2025-01-09 18:54:56 +0800

    fix(yande.re): meta type is not compatible with rule34

--- src/sites/yande/downloadConfigBuilder.ts
+++ src/sites/yande/downloadConfigBuilder.ts
@@ -1,3 +1,36 @@
-import { Rule34DownloadConfig } from '../rule34/downloadConfigBuilder';
+import { ThumbnailButton } from '@/lib/components/Button/thumbnailButton';
+import type { YandeMeta } from './parser';
+import { type DownloadConfig } from '@/lib/downloader';
+import { DownloadConfigBuilder } from '../base/downloadConfigBuilder';
 
-export class YandeDownloadConfig extends Rule34DownloadConfig {}
+function artworkProgressFactory(
+  btn?: ThumbnailButton
+): DownloadConfig<YandeMeta>['onProgress'] | undefined {
+  if (!btn) return;
+
+  return function onArtworkProgress(progress) {
+    btn.setProgress(progress);
+  };
+}
+
+export class YandeDownloadConfig extends DownloadConfigBuilder<YandeMeta> {
+  constructor(protected meta: YandeMeta) {
+    super(meta);
+  }
+
+  public getDownloadConfig(btn?: ThumbnailButton): DownloadConfig<YandeMeta> {
+    return {
+      taskId: this.generateTaskId(),
+      src: this.meta.src,
+      path: this.buildFilePath(),
+      source: this.meta,
+      timeout: 60000,
+      onProgress: artworkProgressFactory(btn)
+    };
+  }
+
+  protected buildFilePath(): string {
+    const path = super.buildFilePath();
+    return path.replaceAll('{character}', this.normalizeString(this.meta.character));
+  }
+}
