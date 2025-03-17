04190642456b8f329efd675a059abc3316ac59a9
ddrunkgoose <ddrunkgoose@protonmail.com>
2025-03-18 04:59:15 +0800

    docs: release 1.8.0

--- src/lib/components/Modal/Changelog/Changelog.svelte
+++ src/lib/components/Modal/Changelog/Changelog.svelte
@@ -21,9 +21,17 @@
   <header class="modal-header text-2xl font-bold">Pixiv Downloader {__VERSION__}</header>
 
   <article class="modal-body mt-4">
+    <h4 class=" text-xl mt-2">新增</h4>
+    <ul class="list-disc list-inside leading-loose">
+      <li>支持新网站：Nijie.info。</li>
+      <li>
+        添加文件名模板：<code class=" code">&#123;score&#125;</code
+        >。注：Pixiv中为收藏数，Nijie中为点赞数。
+      </li>
+    </ul>
     <h4 class=" text-xl mt-2">修复</h4>
     <ul class="list-disc list-inside leading-loose">
-      <li>无法批量下载rule34, safebooru的问题。</li>
+      <li>修复未按正确顺序下载E621 pool的问题。</li>
     </ul>
   </article>
 
