1fae0ead1ee8479049531350e869a15be9bad0c0
ddrunkgoose <ddrunkgoose@protonmail.com>
2025-03-28 07:35:08 +0800

    docs: release 1.8.2

--- src/lib/components/Modal/Changelog/Changelog.svelte
+++ src/lib/components/Modal/Changelog/Changelog.svelte
@@ -21,15 +21,12 @@
   <header class="modal-header text-2xl font-bold">Pixiv Downloader {__VERSION__}</header>
 
   <article class="modal-body mt-4">
+    <p class="mb-2">本次更新修复了测试版Pixiv中的几个问题。</p>
     <h4 class=" text-xl mt-2">修复</h4>
     <ul class="list-disc list-inside leading-loose">
-      <li>
-        无法正常下载E6ai的问题。<a
-          href="https://github.com/drunkg00se/Pixiv-Downloader/issues/33"
-          target="_blank"
-          class=" anchor">(#33)</a
-        >
-      </li>
+      <li>测试版Pixiv中，开启“下载作品时点赞 / 下载作品时收藏”功能时无法下载作品的问题。</li>
+      <li>测试版Pixiv中，预览原图时下载按钮不显示的问题。</li>
+      <li>测试版Pixiv中，仅可由链接浏览作品不显示下载按钮的问题。</li>
     </ul>
   </article>
 
