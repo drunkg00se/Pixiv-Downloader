cb60a0f9d4d6da547d9c3a6ddcfc70337f2cf04c
ddrunkgoose <ddrunkgoose@protonmail.com>
2025-04-12 16:02:42 +0800

    fix: infinite reactivity loop

--- src/lib/store/storage.svelte.ts
+++ src/lib/store/storage.svelte.ts
@@ -1,3 +1,4 @@
+import { channelEvent } from '../channelEvent';
 import { isPlainObject } from '../util';
 import { toStore, type Readable, type Writable } from 'svelte/store';
 
@@ -48,6 +49,9 @@ export function createPersistedStore<
   State extends LocalStorageState,
   Proto extends object & ThisType<State> = object & ThisType<State>
 >(key: string, initialValue: State, proto?: Proto): PersistedStore<State, Proto> {
+  let version = 0;
+  let syncVersion = 0;
+
   const storageValue = localStorage.getItem(key);
 
   let state = $state(
@@ -63,15 +67,26 @@ export function createPersistedStore<
 
   $effect.root(() => {
     $effect(() => {
+      if (version === 0) {
+        version = Date.now();
+      } else if (version < syncVersion) {
+        version = syncVersion;
+      } else {
+        version = Date.now();
+        channelEvent.emit(`sync:${key}`, {
+          version,
+          data: $state.snapshot(state)
+        });
+      }
+
       localStorage.setItem(key, JSON.stringify(state));
     });
   });
 
-  window.addEventListener('storage', (e) => {
-    if (e.storageArea !== localStorage || e.key !== key || !e.newValue) return;
-    console.log('storage change');
-
-    state = JSON.parse(e.newValue);
+  channelEvent.on(`sync:${key}`, ({ version: v, data }: { version: number; data: State }) => {
+    if (v <= version) return;
+    syncVersion = v;
+    state = data;
   });
 
   const store = Object.create(proto ? proto : null) as PersistedStore<State, Proto>;
